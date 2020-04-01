600e88d9ff5d77e4d89c4bdef1e5ac800d02a5ed
keiya01 <keiya.s.0210@gmail.com>
2020-04-01 16:42:41 +0900

    Add minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default {
@@ -11,7 +12,11 @@ export default {
     {
       file: pkg['main'],
       format: 'umd',
-      name: 'AriaModalElement'
+      name: 'AriaModalElement',
+      plugins: [
+        process.env.NODE_ENV === 'production'
+        && terser()
+      ]
     }
   ],
   plugins: [
